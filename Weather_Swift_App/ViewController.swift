9e679ec97589bd12b15da20a7359a48eef293253
Abzal <abzalmanatovich@icloud.com>
2021-04-21 16:04:40 +0600

    Initial Commit

---
+++ Weather_Swift_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_Swift_App
+//
+//  Created by Абзал Айтмолданов on 21.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
